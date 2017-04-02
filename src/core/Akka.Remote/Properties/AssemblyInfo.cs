25839881bccdde3175be841face51b0322da44f6
ravengerUA <yurisikorsky@gmail.com>
2017-04-02 22:25:16 +0300

    moved to dotnet cli tooling

--- src/core/Akka.Remote/Properties/AssemblyInfo.cs
+++ src/core/Akka.Remote/Properties/AssemblyInfo.cs
@@ -13,11 +13,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Akka.Remote")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("Akka.Remote")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -27,8 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("78986bdb-73f7-4532-8e03-1c9ccbe8148e")]
-[assembly: InternalsVisibleTo("Akka.Remote.TestKit.Tests")]
 [assembly: InternalsVisibleTo("Akka.Remote.TestKit")]
+[assembly: InternalsVisibleTo("Akka.Remote.TestKit.Tests")]
 [assembly: InternalsVisibleTo("Akka.Remote.Tests")]
 [assembly: InternalsVisibleTo("Akka.Remote.Tests.MultiNode")]
 [assembly: InternalsVisibleTo("Akka.Remote.Tests.Performance")]
